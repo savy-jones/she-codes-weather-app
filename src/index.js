aab5f3cdbd0e0af7a266bfc5a49bc32bda2f6739
savy-jones <123995270+savy-jones@users.noreply.github.com>
2023-03-01 16:02:35 -0700

    fixed icon url

--- src/index.js
+++ src/index.js
@@ -104,7 +104,7 @@ function displayWeather(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
